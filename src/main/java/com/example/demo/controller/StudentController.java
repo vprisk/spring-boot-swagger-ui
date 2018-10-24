2efbc43b915894b54736029fc36782a81fcebb0e
yuanyuan.jing <991718434@qq.com>
2018-10-24 09:32:19 +0800

    添加查询方法

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,9 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.bean.Student;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Api(description = "学员管理API")
@@ -21,5 +25,11 @@ public class StudentController {
     public Student updateStu(@RequestBody Student student) {
 
         return student;
+    }
+
+    @GetMapping("/student/get")
+    @ApiOperation(value = "获取学员",notes = "从系统中获取所有的学员信息")
+    public List<Student> getAllStudent(){
+        return new ArrayList<>();
     }
 }
